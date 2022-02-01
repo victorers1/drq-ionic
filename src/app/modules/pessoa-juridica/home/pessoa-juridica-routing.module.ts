8a2597611f7460bf9e529a92e38fc8504cd8f602
Victor Emanuel <victor.emanuel@esig.com.br>
2022-02-01 19:50:43 -0300

    remove pages desnecessarias

--- src/app/modules/pessoa-juridica/home/pessoa-juridica-routing.module.ts
+++ src/app/modules/pessoa-juridica/home/pessoa-juridica-routing.module.ts
@@ -16,41 +16,6 @@ const routes: Routes = [
     loadChildren: () =>
       import('../agendar/agendar.module').then((m) => m.AgendarPageModule),
   },
-  {
-    path: drqRoutes.AGENDA,
-    loadChildren: () =>
-      import('../list-agenda/list-agenda.module').then(
-        (m) => m.ListAgendaPageModule
-      ),
-  },
-  {
-    path: drqRoutes.EDIT_DADOS_CONTA,
-    loadChildren: () =>
-      import('../edit-dados-conta/dados-conta.module').then(
-        (m) => m.DadosContaPageModule
-      ),
-  },
-  {
-    path: drqRoutes.LIST_REQUISICOES_DADOS,
-    loadChildren: () =>
-      import('../list-requisicoes-dado/requisicoes.module').then(
-        (m) => m.RequisicoesPageModule
-      ),
-  },
-  {
-    path: drqRoutes.LIST_EXPEDIENTES,
-    loadChildren: () =>
-      import('../list-expedientes/list-expedientes.module').then(
-        (m) => m.ListExpedientesPageModule
-      ),
-  },
-  {
-    path: drqRoutes.LIST_UNIDADES,
-    loadChildren: () =>
-      import('../list-unidades/list-unidades.module').then(
-        (m) => m.ListUnidadesPageModule
-      ),
-  },
 ];
 
 @NgModule({
