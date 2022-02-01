761408f0eaec26ed85fb53aff972c7f5e9812ccb
Victor Emanuel <victor.emanuel@esig.com.br>
2022-01-31 23:06:51 -0300

    remove componentes desnecessarios

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,10 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
-import { DrqEmptyComponent } from './drq-empty/drq-empty.component';
-import { DrqErrorComponent } from './drq-error/drq-error.component';
 import { DrqHeaderComponent } from './drq-header/drq-header.component';
-import { DrqLoadingComponent } from './drq-loading/drq-loading.component';
 import { ProfissionalItemComponent } from './profissional-item/profissional-item.component';
 import { SafeAreaBottomComponent } from './safe-area-bottom/safe-area-bottom.component';
 
@@ -14,17 +11,11 @@ import { SafeAreaBottomComponent } from './safe-area-bottom/safe-area-bottom.com
 @NgModule({
   declarations: [
     DrqHeaderComponent,
-    DrqErrorComponent,
-    DrqLoadingComponent,
-    DrqEmptyComponent,
     SafeAreaBottomComponent,
     ProfissionalItemComponent,
   ],
   exports: [
     DrqHeaderComponent,
-    DrqErrorComponent,
-    DrqLoadingComponent,
-    DrqEmptyComponent,
     SafeAreaBottomComponent,
     ProfissionalItemComponent,
   ],
